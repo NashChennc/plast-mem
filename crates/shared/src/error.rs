25554cfec22c959b683d24d3a2fb0b914b668477
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-02-11 23:23:18 +0800

    refactor(error): improve structure

--- crates/shared/src/error.rs
+++ crates/shared/src/error.rs
@@ -1,27 +1,68 @@
-use std::fmt::Display;
+use std::{
+  backtrace::{Backtrace, BacktraceStatus},
+  fmt::Display,
+};
 
 use axum::{
   http::StatusCode,
   response::{IntoResponse, Response},
 };
 
-// TODO: https://github.com/launchbadge/realworld-axum-sqlx/blob/main/src/http/error.rs
 #[derive(Debug)]
-pub struct AppError(anyhow::Error);
+pub struct AppError {
+  err: anyhow::Error,
+  status_code: StatusCode,
+}
+
+impl AppError {
+  /// Create with 500 status
+  pub fn new<E: Into<anyhow::Error>>(err: E) -> Self {
+    Self {
+      err: err.into(),
+      status_code: StatusCode::INTERNAL_SERVER_ERROR,
+    }
+  }
+
+  /// Create with custom status
+  pub fn with_status<E: Into<anyhow::Error>>(status: StatusCode, err: E) -> Self {
+    Self {
+      err: err.into(),
+      status_code: status,
+    }
+  }
+
+  pub fn status_code(&self) -> StatusCode {
+    self.status_code
+  }
+
+  /// Get backtrace from anyhow (requires RUST_BACKTRACE=1 to capture)
+  pub fn backtrace(&self) -> &Backtrace {
+    self.err.backtrace()
+  }
+}
 
 impl IntoResponse for AppError {
   fn into_response(self) -> Response {
-    (
-      StatusCode::INTERNAL_SERVER_ERROR,
-      format!("Something went wrong: {}", self.0),
-    )
-      .into_response()
+    let body = if cfg!(debug_assertions) {
+      let bt = self.err.backtrace();
+      if bt.status() == BacktraceStatus::Captured {
+        format!("{}\nBacktrace:\n{}", self.err, bt)
+      } else {
+        format!(
+          "{}\n(hint: set RUST_BACKTRACE=1 to enable backtrace)",
+          self.err
+        )
+      }
+    } else {
+      self.err.to_string()
+    };
+    (self.status_code, body).into_response()
   }
 }
 
 impl Display for AppError {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    self.0.fmt(f)
+    write!(f, "[{}] {}", self.status_code, self.err)
   }
 }
 
@@ -30,6 +71,6 @@ where
   E: Into<anyhow::Error>,
 {
   fn from(err: E) -> Self {
-    Self(err.into())
+    Self::new(err)
   }
 }
